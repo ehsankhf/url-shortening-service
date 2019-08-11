d1f44e8bf6310a2707e0512b97832f8e65410657
ParanormalCoder <ehsan.khf@gmail.com>
2019-08-11 09:22:25 +0100

    Vuex store configuration

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
-})
+export default {
+    state: {
+        urls: [],
+        currentShortUrl: ''
+    },
+    mutations: {
+        SET_URLS: (state, urls) => (state.urls = urls),
+        SET_CURRENT_SHORT_URL: (state, currentShortUrl) => (state.currentShortUrl = currentShortUrl)
+    },
+    actions: {
+        getUrls: ({ commit }) => {
+            return axios.get("http://localhost:3000/todos").then(response => {
+                commit("SET_URLS", response.data);
+            });
+        },
+        getShortUrl: ({ commit }, url) => {
+            return axios.post("http://localhost:3000/todos", { url }).then(response => {
+                commit("SET_CURRENT_SHORT_URL", `http://localhost:3000/short/${response.data.short_url_id}`);
+            });
+        }
+    },
+    getters: {
+        urls: state => state.urls,
+        currentShortUrl: state => state.currentShortUrl
+    }
+}
---
+++ client/tests/unit/store.spec.js
@@ -0,0 +1,62 @@
+import sinon from 'sinon';
+import moxios from 'moxios'
+
+import Store from '../../src/store';
+
+// destructure assign `mutations`
+const { SET_URLS, SET_CURRENT_SHORT_URL } = Store.mutations
+const { getShortUrl, getUrls } = Store.actions
+
+describe('store', () => {
+    describe('mutations', () => {
+        it('SET_URLS', () => {
+            const urls = [{ url: 'yahoo.com', short_url_id: '2232323' }]
+            const state = { urls: [] }
+            SET_URLS(state, urls)
+            expect(state.urls).toBe(urls)
+        })
+
+        it('SET_CURRENT_SHORT_URL', () => {
+            const currentShortUrl = 'yahoo.com'
+            const state = { currentShortUrl: '' }
+            SET_CURRENT_SHORT_URL(state, currentShortUrl)
+            expect(state.currentShortUrl).toBe('yahoo.com')
+        })
+    })
+
+    describe('actions', () => {
+        it('getUrls', async () => {
+            const commit = sinon.spy()
+
+            moxios.install()
+            moxios.stubRequest('http://localhost:3000/todos', {
+                status: 200,
+                response: [{ url: 'yahoo.com', short_url_id: '233232323' }]
+            });
+
+            await getUrls({ commit })
+            expect(commit.args[0]).toEqual(
+                ['SET_URLS', [{ url: 'yahoo.com', short_url_id: '233232323' }]],
+            )
+
+            moxios.uninstall()
+        })
+
+        it('getShortUrl', async () => {
+            const commit = sinon.spy()
+
+            moxios.install()
+            moxios.stubRequest('http://localhost:3000/todos', {
+                status: 200,
+                response: { url: 'yahoo123.com', short_url_id: '233232323' }
+            });
+
+            await getShortUrl({ commit }, 'yahoo123.com')
+            expect(commit.args[0]).toEqual(
+                ['SET_CURRENT_SHORT_URL', `http://localhost:3000/short/233232323`]
+            )
+
+            moxios.uninstall()
+        })
+    })
+})
